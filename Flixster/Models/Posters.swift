d1dfe8c0affa38ad8ebf2e84c819dba2f4eaa13c
CastroJ12 <chepe.castro2312@gmail.com>
2023-02-23 22:41:38 -0800

    Update xcodeproj fie

---
+++ Flixster/Models/Posters.swift
@@ -0,0 +1,8 @@
+//
+//  Posters.swift
+//  Flixster
+//
+//  Created by Jose Castro on 2/23/23.
+//
+
+import Foundation
---
+++ Flixster/PosterCell.swift
@@ -0,0 +1,12 @@
+//
+//  PosterCell.swift
+//  Flixster
+//
+//  Created by Jose Castro on 2/23/23.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+}
